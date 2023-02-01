7296a1b4ae2b1917e3def8a4f60d4d7908f8ded4
jinhyuk <kjh456879@naver.com>
2023-02-01 22:28:41 +0900

    bcrypt, jsonwebtoken, cookieParser 추가

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const config = require('./config/key');
 
 const { User } = require('./model/User');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
@@ -14,6 +15,8 @@ app.use(bodyParser.urlencoded({
 
 //application/json
 app.use(bodyParser.json());
+
+app.use(cookieParser());
 
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
@@ -35,11 +38,38 @@ app.post('/register', (req, res) => {
     if(err) return res.json({success: false, err})
     return res.status(200).json({
       success: true
+
     })
   });
-
-
 })
+
+app.post('/login', (req, res) => {
+  //요청된 이메일을 데이터베이스에서 있는지 찾음
+  User.findOne({email: req.body.email}, (err, user) => {
+    if(!user){
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+
+    //요청된 이메일이 데이터베이스에 있다면 비밀번호 맞는 비밀번호인지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch)
+        return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+
+      //비밀번호까지 맞다면 토큰 생성
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err)
+        
+        //토큰 저장 (쿠키, 로컬 스토리지, 세션)
+        res.cookie("x_auth", user.token).status(200).json({loginSuccess: true, userId: user._id})
+      })
+    })
+  })
+  
+  
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
